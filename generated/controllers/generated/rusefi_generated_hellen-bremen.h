7d80ae3f04b3e47fb117b64b922823309c1571ea
GitHub git update Action <action@github.com>
2025-07-14 12:54:27 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_hellen-bremen.h
+++ generated/controllers/generated/rusefi_generated_hellen-bremen.h
@@ -1437,7 +1437,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2176470625
+#define SIGNATURE_HASH 492596860
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1820,7 +1820,7 @@
 #define ts_show_etb_pins true
 #define ts_show_exhaust_vvt true
 #define ts_show_experimental true
-#define ts_show_fan2_settings false
+#define ts_show_fan2_settings true
 #define ts_show_faster_spin_up true
 #define ts_show_final_ratio true
 #define ts_show_firing_order true
@@ -1955,7 +1955,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.07.13.hellen-bremen.2176470625"
+#define TS_SIGNATURE "rusEFI main.2025.07.14.hellen-bremen.492596860"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
