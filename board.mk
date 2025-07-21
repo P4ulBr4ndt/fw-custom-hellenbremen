31a55f42e28c22d9e9801392d3c6867873b71115
Paul Brandt <pbrandt1995@gmail.com>
2025-07-21 14:54:56 +0200

    working on ADC and BT stuff

--- board.mk
+++ board.mk
@@ -6,17 +6,16 @@ BOARDINC += $(BOARD_DIR)/generated/controllers/generated
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
-# this is usually used by Bluetooth
-DDEFS += $(PRIMARY_COMMUNICATION_PORT_USART2)
-
 # this includes knock and mux control settings
 include $(BOARDS_DIR)/hellen/hellen-common-mega144.mk
 
 # ACR Management
 DDEFS += -DEFI_HD_ACR=TRUE
 
-# This stuff doesn't work on H7 yet
-ifneq ($(PROJECT_CPU),ARCH_STM32H7)
-	DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
-	DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE
-endif
+# We want to use ADC3 for knock sampling
+DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
+DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE
+
+# We want bluetooth
+DDEFS += -DEFI_BLUETOOTH_SETUP=TRUE
+DDEFS += $(PRIMARY_COMMUNICATION_PORT_USART2)
--- knock_config.h
+++ knock_config.h
@@ -7,9 +7,14 @@
 // Knock is on ADC3
 #define KNOCK_ADC ADCD3
 
-// knock 1 - pin PA3
+// knock 1 - pin PC5
 #define KNOCK_ADC_CH1 ADC_CHANNEL_IN3
-#define KNOCK_PIN_CH1 Gpio::A3
+#define KNOCK_PIN_CH1 Gpio::C5
+
+// knock 2 - pin PB1
+#define KNOCK_HAS_CH2 true
+#define KNOCK_ADC_CH2 ADC_CHANNEL_IN3
+#define KNOCK_PIN_CH2 Gpio::B1
 
 // Sample rate & time - depends on the exact MCU
 #define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
