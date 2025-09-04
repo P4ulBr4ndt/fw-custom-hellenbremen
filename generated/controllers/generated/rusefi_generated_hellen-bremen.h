b88f132448b7151db7b8d3b53317a6a04dd3a0aa
GitHub git update Action <action@github.com>
2025-09-04 16:45:56 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_hellen-bremen.h
+++ generated/controllers/generated/rusefi_generated_hellen-bremen.h
@@ -1949,7 +1949,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.09.03.hellen-bremen.3557528371"
+#define TS_SIGNATURE "rusEFI main.2025.09.04.hellen-bremen.3557528371"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
