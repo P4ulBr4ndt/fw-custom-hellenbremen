bd9911ecddc4aa5b7cd3fe4460192d799d983ef8
fwillo <felix@willenborg.io>
2026-05-08 13:44:11 +0200

    Fixing a logical error, resetting the timer the whole time; adding a efiprintf when time is reached

--- board_can.cpp
+++ board_can.cpp
@@ -248,21 +248,25 @@ void boardPeriodicSlow() {
 	if(engine->rpmCalculator.isRunning() && !prgselWarmupTimeFinished) {
 		boardPeriodicSlowEngineRuntimeCounter++;
 
-		if(boardPeriodicSlowEngineRuntimeCounter >= (uint32_t)(180 / 0.05f))
+		if(boardPeriodicSlowEngineRuntimeCounter >= (uint32_t)(180 / 0.05f)) {
 			prgselWarmupTimeFinished = true;
-		else
-			boardPeriodicSlowEngineRuntimeCounter = 0;
+			efiprintf("Purge Solenoid Warm Up time reached!");
+		}
+	} else {
+		boardPeriodicSlowEngineRuntimeCounter = 0;
 	}
+	
 
 	if((Sensor::getOrZero(SensorType::Rpm) >= 2000.0f) &&
 	   (Sensor::getOrZero(SensorType::VehicleSpeed) >= 10.0f) &&
 	   (Sensor::getOrZero(SensorType::AcceleratorPedal) >= 5.0f) &&
 	   (Sensor::getOrZero(SensorType::AcceleratorPedal) <= 50.0f) &&
 	   (Sensor::getOrZero(SensorType::Clt) >= 90.0f) && 
-	    prgselWarmupTimeFinished)
+	    prgselWarmupTimeFinished) {
 		prgselPwm.setFrequency(32.0f);
-	else
+	} else {
 		prgselPwm.setFrequency(NAN); // setFrequecy(NAN) deactivates the PWM schedule
+	}
 }
 
 void boardHandleCan(CanCycle cycle) {
