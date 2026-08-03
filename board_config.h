c6609e146117f393b88635bf5091337dc2d74593
fwillo <felix@willenborg.io>
2026-08-03 19:13:13 +0200

    Remove redundant function

--- board_config.h
+++ board_config.h
@@ -20,6 +20,3 @@ void boardConfigOverrides();
 void boardCustomInitHardware();
 void boardHandleTsCommand(uint16_t subsystem, uint16_t index);
 void boardCustomOnConfigurationChange(engine_configuration_s* previousConfiguration);
-
-// Returns true exactly once after a TS calibration write is observed, then clears itself.
-bool consumeCalibrationWriteSignal();
